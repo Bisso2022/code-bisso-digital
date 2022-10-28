e225977caed47d535d6d4f9e8ffd24dbbc91091a
Bisso Digital <116199106+Bisso2022@users.noreply.github.com>
2022-10-28 02:33:11 +0100

    agenceweb\vendor\barryvdh\laravel-dompdf\tests

--- TestCase.php
+++ TestCase.php
@@ -1,10 +1,37 @@
 <?php
 
-namespace Tests;
+namespace Barryvdh\DomPDF\Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Barryvdh\DomPDF\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    use CreatesApplication;
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        View::addLocation(__DIR__.'/views');
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     * @return string[]
+     */
+    protected function getPackageProviders($app)
+    {
+        return [ServiceProvider::class];
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     * @return string[]
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'PDF' => Pdf::class,
+        ];
+    }
 }
