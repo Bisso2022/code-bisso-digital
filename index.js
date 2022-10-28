1391dd198da7ceae19d97ea87e39dcf917078479
Bisso Digital <116199106+Bisso2022@users.noreply.github.com>
2022-10-28 01:38:22 +0100

    agenceweb\node_modules\@babel\helper-function-name\lib

--- index.js
+++ index.js
@@ -5,88 +5,194 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = _default;
 
+var _template = require("@babel/template");
+
 var _t = require("@babel/types");
 
 const {
-  assignmentExpression,
+  NOT_LOCAL_BINDING,
   cloneNode,
+  identifier,
+  isAssignmentExpression,
+  isAssignmentPattern,
+  isFunction,
   isIdentifier,
   isLiteral,
-  isMemberExpression,
-  isPrivateName,
-  isPureish,
-  isSuper,
-  memberExpression,
-  toComputedKey
+  isNullLiteral,
+  isObjectMethod,
+  isObjectProperty,
+  isRegExpLiteral,
+  isRestElement,
+  isTemplateLiteral,
+  isVariableDeclarator,
+  toBindingIdentifierName
 } = _t;
 
-function getObjRef(node, nodes, scope) {
-  let ref;
+function getFunctionArity(node) {
+  const count = node.params.findIndex(param => isAssignmentPattern(param) || isRestElement(param));
+  return count === -1 ? node.params.length : count;
+}
 
-  if (isIdentifier(node)) {
-    if (scope.hasBinding(node.name)) {
-      return node;
+const buildPropertyMethodAssignmentWrapper = (0, _template.default)(`
+  (function (FUNCTION_KEY) {
+    function FUNCTION_ID() {
+      return FUNCTION_KEY.apply(this, arguments);
+    }
+
+    FUNCTION_ID.toString = function () {
+      return FUNCTION_KEY.toString();
+    }
+
+    return FUNCTION_ID;
+  })(FUNCTION)
+`);
+const buildGeneratorPropertyMethodAssignmentWrapper = (0, _template.default)(`
+  (function (FUNCTION_KEY) {
+    function* FUNCTION_ID() {
+      return yield* FUNCTION_KEY.apply(this, arguments);
+    }
+
+    FUNCTION_ID.toString = function () {
+      return FUNCTION_KEY.toString();
+    };
+
+    return FUNCTION_ID;
+  })(FUNCTION)
+`);
+const visitor = {
+  "ReferencedIdentifier|BindingIdentifier"(path, state) {
+    if (path.node.name !== state.name) return;
+    const localDeclar = path.scope.getBindingIdentifier(state.name);
+    if (localDeclar !== state.outerDeclar) return;
+    state.selfReference = true;
+    path.stop();
+  }
+
+};
+
+function getNameFromLiteralId(id) {
+  if (isNullLiteral(id)) {
+    return "null";
+  }
+
+  if (isRegExpLiteral(id)) {
+    return `_${id.pattern}_${id.flags}`;
+  }
+
+  if (isTemplateLiteral(id)) {
+    return id.quasis.map(quasi => quasi.value.raw).join("");
+  }
+
+  if (id.value !== undefined) {
+    return id.value + "";
+  }
+
+  return "";
+}
+
+function wrap(state, method, id, scope) {
+  if (state.selfReference) {
+    if (scope.hasBinding(id.name) && !scope.hasGlobal(id.name)) {
+      scope.rename(id.name);
     } else {
-      ref = node;
-    }
-  } else if (isMemberExpression(node)) {
-    ref = node.object;
+      if (!isFunction(method)) return;
+      let build = buildPropertyMethodAssignmentWrapper;
 
-    if (isSuper(ref) || isIdentifier(ref) && scope.hasBinding(ref.name)) {
-      return ref;
+      if (method.generator) {
+        build = buildGeneratorPropertyMethodAssignmentWrapper;
+      }
+
+      const template = build({
+        FUNCTION: method,
+        FUNCTION_ID: id,
+        FUNCTION_KEY: scope.generateUidIdentifier(id.name)
+      }).expression;
+      const params = template.callee.body.body[0].params;
+
+      for (let i = 0, len = getFunctionArity(method); i < len; i++) {
+        params.push(scope.generateUidIdentifier("x"));
+      }
+
+      return template;
     }
-  } else {
-    throw new Error(`We can't explode this node type ${node["type"]}`);
   }
 
-  const temp = scope.generateUidIdentifierBasedOnNode(ref);
-  scope.push({
-    id: temp
-  });
-  nodes.push(assignmentExpression("=", cloneNode(temp), cloneNode(ref)));
-  return temp;
+  method.id = id;
+  scope.getProgramParent().references[id.name] = true;
 }
 
-function getPropRef(node, nodes, scope) {
-  const prop = node.property;
-
-  if (isPrivateName(prop)) {
-    throw new Error("We can't generate property ref for private name, please install `@babel/plugin-proposal-class-properties`");
-  }
-
-  const key = toComputedKey(node, prop);
-  if (isLiteral(key) && isPureish(key)) return key;
-  const temp = scope.generateUidIdentifierBasedOnNode(prop);
-  scope.push({
-    id: temp
-  });
-  nodes.push(assignmentExpression("=", cloneNode(temp), cloneNode(prop)));
-  return temp;
-}
-
-function _default(node, nodes, file, scope, allowedSingleIdent) {
-  let obj;
-
-  if (isIdentifier(node) && allowedSingleIdent) {
-    obj = node;
-  } else {
-    obj = getObjRef(node, nodes, scope);
-  }
-
-  let ref, uid;
-
-  if (isIdentifier(node)) {
-    ref = cloneNode(node);
-    uid = obj;
-  } else {
-    const prop = getPropRef(node, nodes, scope);
-    const computed = node.computed || isLiteral(prop);
-    uid = memberExpression(cloneNode(obj), cloneNode(prop), computed);
-    ref = memberExpression(cloneNode(obj), cloneNode(prop), computed);
-  }
-
-  return {
-    uid: uid,
-    ref: ref
+function visit(node, name, scope) {
+  const state = {
+    selfAssignment: false,
+    selfReference: false,
+    outerDeclar: scope.getBindingIdentifier(name),
+    references: [],
+    name: name
   };
+  const binding = scope.getOwnBinding(name);
+
+  if (binding) {
+    if (binding.kind === "param") {
+      state.selfReference = true;
+    } else {}
+  } else if (state.outerDeclar || scope.hasGlobal(name)) {
+    scope.traverse(node, visitor, state);
+  }
+
+  return state;
+}
+
+function _default({
+  node,
+  parent,
+  scope,
+  id
+}, localBinding = false, supportUnicodeId = false) {
+  if (node.id) return;
+
+  if ((isObjectProperty(parent) || isObjectMethod(parent, {
+    kind: "method"
+  })) && (!parent.computed || isLiteral(parent.key))) {
+    id = parent.key;
+  } else if (isVariableDeclarator(parent)) {
+    id = parent.id;
+
+    if (isIdentifier(id) && !localBinding) {
+      const binding = scope.parent.getBinding(id.name);
+
+      if (binding && binding.constant && scope.getBinding(id.name) === binding) {
+        node.id = cloneNode(id);
+        node.id[NOT_LOCAL_BINDING] = true;
+        return;
+      }
+    }
+  } else if (isAssignmentExpression(parent, {
+    operator: "="
+  })) {
+    id = parent.left;
+  } else if (!id) {
+    return;
+  }
+
+  let name;
+
+  if (id && isLiteral(id)) {
+    name = getNameFromLiteralId(id);
+  } else if (id && isIdentifier(id)) {
+    name = id.name;
+  }
+
+  if (name === undefined) {
+    return;
+  }
+
+  if (!supportUnicodeId && isFunction(node) && /[\uD800-\uDFFF]/.test(name)) {
+    return;
+  }
+
+  name = toBindingIdentifierName(name);
+  id = identifier(name);
+  id[NOT_LOCAL_BINDING] = true;
+  const state = visit(node, name, scope);
+  return wrap(state, node, id, scope) || node;
 }
