768a1766738e882de52f8eb9b966abed1792a41e
Bisso Digital <116199106+Bisso2022@users.noreply.github.com>
2022-10-28 01:34:47 +0100

    agenceweb\node_modules\@babel\helper-define-polyfill-provider\src\node

--- dependencies.js
+++ dependencies.js
@@ -1,5 +1,16 @@
 // @flow
 
+import path from "path";
+import debounce from "lodash.debounce";
+import requireResolve from "resolve";
+
+const nativeRequireResolve = parseFloat(process.versions.node) >= 8.9;
+
+// $FlowIgnore
+import { createRequire } from "module";
+// $FlowIgnore
+const require = createRequire(import/*::(_)*/.meta.url); // eslint-disable-line
+
 export function resolve(
   dirname: string,
   moduleName: string,
@@ -7,18 +18,74 @@ export function resolve(
 ): string {
   if (absoluteImports === false) return moduleName;
 
-  throw new Error(
-    `"absoluteImports" is not supported in bundles prepared for the browser.`,
-  );
+  let basedir = dirname;
+  if (typeof absoluteImports === "string") {
+    basedir = path.resolve(basedir, absoluteImports);
+  }
+
+  try {
+    if (nativeRequireResolve) {
+      return require.resolve(moduleName, {
+        paths: [basedir],
+      });
+    } else {
+      return requireResolve.sync(moduleName, { basedir });
+    }
+  } catch (err) {
+    if (err.code !== "MODULE_NOT_FOUND") throw err;
+
+    // $FlowIgnore
+    throw Object.assign(
+      new Error(`Failed to resolve "${moduleName}" relative to "${dirname}"`),
+      {
+        code: "BABEL_POLYFILL_NOT_FOUND",
+        polyfill: moduleName,
+        dirname,
+      },
+    );
+  }
 }
 
-// eslint-disable-next-line no-unused-vars
 export function has(basedir: string, name: string) {
-  return true;
+  try {
+    if (nativeRequireResolve) {
+      require.resolve(name, { paths: [basedir] });
+    } else {
+      requireResolve.sync(name, { basedir });
+    }
+    return true;
+  } catch {
+    return false;
+  }
 }
 
-// eslint-disable-next-line no-unused-vars
-export function logMissing(missingDeps: Set<string>) {}
+export function logMissing(missingDeps: Set<string>) {
+  if (missingDeps.size === 0) return;
 
-// eslint-disable-next-line no-unused-vars
-export function laterLogMissing(missingDeps: Set<string>) {}
+  const deps = Array.from(missingDeps)
+    .sort()
+    .join(" ");
+
+  console.warn(
+    "\nSome polyfills have been added but are not present in your dependencies.\n" +
+      "Please run one of the following commands:\n" +
+      `\tnpm install --save ${deps}\n` +
+      `\tyarn add ${deps}\n`,
+  );
+
+  process.exitCode = 1;
+}
+
+let allMissingDeps = new Set();
+
+const laterLogMissingDependencies = debounce(() => {
+  logMissing(allMissingDeps);
+  allMissingDeps = new Set();
+}, 100);
+
+export function laterLogMissing(missingDeps: Set<string>) {
+  if (missingDeps.size === 0) return;
+
+  missingDeps.forEach(name => allMissingDeps.add(name));
+  laterLogMissingDependencies();
+}
